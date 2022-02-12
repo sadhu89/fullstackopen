70ccad5e22521b34fed02142c0c83409816114e3
Carlos Rojas <carlos.sadhu@gmail.com>
2022-02-12 19:00:30 +1100

    unicafe step 4

--- part1/unicafe/src/Statistics.js
+++ part1/unicafe/src/Statistics.js
@@ -1,15 +1,27 @@
 const Statistics = ({good, neutral, bad}) => {
   const total = good + neutral + bad
+  const noFeedback = (
+      <p>
+        No feedback given
+      </p>
+    )
+
+  const body = (
+      <>
+        <div>good {good} </div>
+        <div>neutral {neutral}</div>
+        <div>bad {bad}</div>
+        <div>all {total}</div>
+        <div>average {total/3}</div>
+        <div>positive {(good/total)*100} %</div>
+      </>
+  )
+
 
   return (
     <>
       <h2>statistics</h2>
-      <div>good {good} </div>
-      <div>neutral {neutral}</div>
-      <div>bad {bad}</div>
-      <div>all {total}</div>
-      <div>average {total/3}</div>
-      <div>positive {(good/total)*100} %</div>
+      { total === 0 ? noFeedback : body }
     </>
   )
 }
